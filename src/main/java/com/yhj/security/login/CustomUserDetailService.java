8aea0efbb3db4d3101e05da2aa5574cea55659be
yanghj <1501587673@qq.com>
2018-07-17 08:34:17 +0800

    update password encoder

--- src/main/java/com/yhj/security/login/CustomUserDetailService.java
+++ src/main/java/com/yhj/security/login/CustomUserDetailService.java
@@ -1,9 +1,8 @@
 package com.yhj.security.login;
 
 import com.yhj.web.dao.sys.SysUserMapper;
-import com.yhj.web.dao.sys.SysUserRoleMapper;
+import com.yhj.web.entity.sys.SysRole;
 import com.yhj.web.entity.sys.SysUser;
-import com.yhj.web.entity.sys.SysUserRole;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +26,6 @@ public class CustomUserDetailService implements UserDetailsService {
     @Autowired
     private SysUserMapper sysUserMapper;
 
-    @Autowired
-    private SysUserRoleMapper sysUserRoleMapper;
-
 
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String sysUserName) {
@@ -48,12 +44,12 @@ public class CustomUserDetailService implements UserDetailsService {
 
     private List<GrantedAuthority> getGrantedAuthorities(SysUser sysUser) {
         List<GrantedAuthority> authorities = new ArrayList<>();
-        List<SysUserRole> roles = sysUserRoleMapper.querySysUserRoleByName(sysUser);
-        for (SysUserRole sysUserRole : roles) {
+        List<SysRole> roles = sysUser.getRoles();
+        for (SysRole role : roles) {
             if (logger.isDebugEnabled()) {
-                logger.debug("sysUserRole:\t {} \t {}", sysUserRole.getUserName(), sysUserRole.getRoleName());
+                logger.debug("sysUserRole:\t {} \t {}", sysUser.getUserName(), role.getRoleName());
             }
-            authorities.add(new SimpleGrantedAuthority(sysUserRole.getRoleName()));
+            authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
         }
         return authorities;
     }
