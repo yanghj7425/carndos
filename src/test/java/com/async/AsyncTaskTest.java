1cbefe125303729a8f547ee4dbac24a9f48c4cef
yanghj7425 <1501587673@qq.com>
2019-06-20 18:10:19 +0800

    fix badly smell

--- src/test/java/com/async/AsyncTaskTest.java
+++ src/test/java/com/async/AsyncTaskTest.java
@@ -1,20 +1,55 @@
 package com.async;
 
-import com.carndos.config.sync.TaskExecutorConfig;
-import com.carndos.modules.demo.async.AsyncTaskService;
 import org.junit.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class AsyncTaskTest {
 
 
     @Test
-    public void asyncTest() {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TaskExecutorConfig.class);
-        AsyncTaskService taskService = (AsyncTaskService) context.getBean("asyncTaskService");
-        for (int i = 0; i < 20; i++) {
-            taskService.executeAsyncTask(i);
-            taskService.executeAsyncTaskPlus(i);
+    public void asyncTest() throws InterruptedException {
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TaskExecutorConfig.class);
+//        AsyncTaskService taskService = (AsyncTaskService) context.getBean("asyncTaskService");
+//        for (int i = 0; i < 20; i++) {
+//            taskService.executeAsyncTask(i);
+//            taskService.executeAsyncTaskPlus(i);
+//        }
+
+
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        executorService.submit(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread 1");
+            countDownLatch.countDown();
+        });
+
+        executorService.submit(() -> {
+            System.out.println("Thread 2");
+            countDownLatch.countDown();
+
+        });
+
+
+        executorService.shutdown();
+        if (!executorService.isShutdown()) {
+            executorService.awaitTermination(10, TimeUnit.SECONDS);
+            executorService.shutdownNow();
         }
+
+
+        System.out.println("execute finish");
+
+
     }
 }
